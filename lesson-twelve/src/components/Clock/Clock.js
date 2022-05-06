56ca7ccc14555bced7e71875583d85705c384f19
andrey <quesandans491@gmail.com>
2022-05-06 20:30:28 +0300

    lesson-twelve переписать классовый компонент в функцианальный с использованием хуков

---
+++ lesson-twelve/src/components/Clock/Clock.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+
+//class Clock extends React.Component {
+//  constructor(props) {
+//    super(props);
+//    this.state = { date: new Date() };
+//  }
+
+//  componentDidMount() {
+//    this.timerID = setInterval(
+//      () => this.tick(),
+//      1000
+//    );
+//  }
+
+//  componentWillUnmount() {
+//    clearInterval(this.timerID);
+//  }
+
+//  tick() {
+//    this.setState({
+//      date: new Date()
+//    });
+//  }
+
+//  render() {
+//    return (
+//      <div>
+//        <h1>Привет, мир!</h1>
+//        <h2>Сейчас {this.state.date.toLocaleTimeString()}.</h2>
+//      </div>
+//    );
+//  }
+//}
+
+function Clock() {
+
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    setInterval(
+      () => setDate(new Date()),
+      1000
+    );
+  });
+
+  return (
+    <div>
+      <h1>Привет, мир!</h1>
+      <h2>Сейчас {date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+export default Clock;
